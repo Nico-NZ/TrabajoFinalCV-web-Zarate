ab4a9c064c99bc679da21489a563ff649656f7ea
Nico-NZ <z.nicolas.n@gmail.com>
2023-02-25 23:21:31 -0300

    Este es un CV-trabajo final de web

---
+++ js/js practica.js
@@ -0,0 +1,9 @@
+
+
+function myFunction() {
+    let x = document.getElementById("ver").innerHTML=" 'Pregúntate si lo que estás haciendo hoy te acerca al lugar en el que quieres estar mañana.' WALT DISNEY ";
+  
+   
+  }
+
+ 
